bf92e0f5112d7597ac99a017fe5c49580d9ec9f6
ranthy <realthingshakes25@gmail.com>
2015-09-03 21:25:41 +0900

    fixed indent

--- sample.php
+++ sample.php
@@ -21,7 +21,6 @@ $AFFILIATE_ID = "144a9906.be5344cb.144a9907.16e937fa";
 
 // --------- API毎の固定値
 // API名
-//$API_NAME = "楽天商品検索API";
 $API_NAME = "楽天商品検索API";
 
 // APIのURL
@@ -30,11 +29,6 @@ $API_BASE_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/201402
 // --------- リクエストパラメタの取得とAPIへのリクエストURL生成
 
 // リクエストURL生成
-/*$query = array(
-  'format' => "json",
-  'keyword' => $_REQUEST['keyword'],
-  'applicationId' => $APP_ID
-  );*/
 $query = array(
         'format' => "json",
         'keyword' => $_REQUEST['keyword'],
@@ -63,67 +57,67 @@ if (isset($_REQUEST['submit']) && $_REQUEST['submit'] == "商品検索") {
 header("Content-type:text/html;charset=UTF-8");
 ?>
 <html lang="ja">
-        <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title><?php echo $API_NAME; ?>／楽天ウェブサービス</title>
-</head>
-<body bgcolor="#ffffff  " TEXT="#333333  " LINK="#3333cc  ">
-
-          <!-- タイトル -->
-          <h1 style="font-size: 16px; font-weight: bold;">楽天ウェブサービス</h1>
-          <hr size="1" noshade><?php echo $API_NAME; ?><hr size="1" noshade>
-          <!--/タイトル -->
-
-          <!-- HTMLフォーム表示 -->
-          <form action="./sample.php" method="post">
-            <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 5px 0pt 0pt 0px;">
-              <tr>
-                <td bgcolor="#afafaf  ">
-                  <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
-                    <tr>
-                      <td style="background-color: #eeeeee  ;">検索キーワード</td>
-                      <td style="background-color: #ffffff  ;">
-                        <!-- キーワード入力テキストボックス --> <input type="text" name="keyword" value="<?php echo htmlspecialchars($_REQUEST['keyword']); ?>" size="30">
-                      </td>
-                    </tr>
-                  </table>
-                </td>
-              </tr>
-            </table>
-            <br> <input type="submit" name="submit" value="商品検索">
-          </form>
-          <!-- HTMLフォーム表示 -->
-
-          <!-- API検索結果表示 -->
-          <font style="font-size: 14px;">
-<?php if ($count > 0) : ?>
-取得件数: <?php echo number_format($count); ?>件<br>
-    <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 5px 0pt 0pt 0px;">
-              <tr>
-                <td bgcolor="#afafaf  ">
-                  <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
-                    <tr align="center" style="background-color: #eeeeee  ;">
-                      <td width="8%">写真</td>
-                      <td width="47%">商品名</td>
-                      <td width="15%">価格</td>
-                      <td width="30%">ショップ名</td>
-                    </tr>
-<?php foreach ($item_list as $data) : ?>
-<?php $item = $data['Item']; ?>
-                    <tr style="background-color: #ffffff  ;">
-                      <td width="8%" align="center"><a href="<?php echo $item['itemUrl']; ?>" target="_top"><img src="<?php echo $item['smallImageUrls'][0]['imageUrl']; ?>" border=0></a></td>
-                      <td width="47%" align="left"><a href="<?php echo $item['itemUrl']; ?>" target="_top"><font size="-1"><?php echo $item['itemName']; ?></a></td>
-              <td width="15%" align="right" nowrap> <?php echo number_format($item['itemPrice']); ?> 円</td>
-              <td width="47%" align="left"><a href="<?php echo $item['shopUrl']; ?>" target="_top"><font size="-1"><?php echo $item['shopName']; ?></a></td>
-            </tr>
-<?php endforeach; ?>
-          </table>
-        </td>
-      </tr>
-    </table>
-<?php endif; ?>
-</font>
-  <!-- /API検索結果表示 -->
-
-          </body>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title><?php echo $API_NAME; ?>／楽天ウェブサービス</title>
+  </head>
+  <body bgcolor="#ffffff  " TEXT="#333333  " LINK="#3333cc  ">
+    
+      <!-- タイトル -->
+      <h1 style="font-size: 16px; font-weight: bold;">楽天ウェブサービス</h1>
+      <hr size="1" noshade><?php echo $API_NAME; ?><hr size="1" noshade>
+      <!--/タイトル -->
+    
+      <!-- HTMLフォーム表示 -->
+      <form action="./sample.php" method="post">
+          <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 5px 0pt 0pt 0px;">
+	      <tr>
+	          <td bgcolor="#afafaf  ">
+	              <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
+	                  <tr>
+		              <td style="background-color: #eeeeee  ;">検索キーワード</td>
+		              <td style="background-color: #ffffff  ;">
+		                  <!-- キーワード入力テキストボックス --> <input type="text" name="keyword" value="<?php echo htmlspecialchars($_REQUEST['keyword']); ?>" size="30">
+		              </td>
+	                  </tr>
+	              </table>
+	          </td>
+	      </tr>
+          </table>
+          <br> <input type="submit" name="submit" value="商品検索">
+      </form>
+      <!-- HTMLフォーム表示 -->
+    
+      <!-- API検索結果表示 -->
+      <font style="font-size: 14px;">
+      <?php if ($count > 0) : ?>
+	取得件数: <?php echo number_format($count); ?>件<br>
+	    <table width="60%" border="0" cellspacing="0" cellpadding="0" style="margin: 5px 0pt 0pt 0px;">
+	        <tr>
+	            <td bgcolor="#afafaf  ">
+	                <table width="100%" border=0 cellspacing=1 cellpadding=5 style="font-size: 12px;">
+		            <tr align="center" style="background-color: #eeeeee  ;">
+		                <td width="8%">写真</td>
+		                <td width="47%">商品名</td>
+		                <td width="15%">価格</td>
+		                <td width="30%">ショップ名</td>
+		            </tr>
+		<?php foreach ($item_list as $data) : ?>
+		  <?php $item = $data['Item']; ?>
+		              <tr style="background-color: #ffffff  ;">
+		                  <td width="8%" align="center"><a href="<?php echo $item['itemUrl']; ?>" target="_top"><img src="<?php echo $item['smallImageUrls'][0]['imageUrl']; ?>" border=0></a></td>
+		                  <td width="47%" align="left"><a href="<?php echo $item['itemUrl']; ?>" target="_top"><font size="-1"><?php echo $item['itemName']; ?></a></td>
+		                    <td width="15%" align="right" nowrap> <?php echo number_format($item['itemPrice']); ?> 円</td>
+		                    <td width="47%" align="left"><a href="<?php echo $item['shopUrl']; ?>" target="_top"><font size="-1"><?php echo $item['shopName']; ?></a></td>
+		              </tr>
+		<?php endforeach; ?>
+	                </table>
+	            </td>
+	        </tr>
+	    </table>
+      <?php endif; ?>
+		      </font>
+		        <!-- /API検索結果表示 -->
+		      
+  </body>
 </html>
